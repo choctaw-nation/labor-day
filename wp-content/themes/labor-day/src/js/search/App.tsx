7e9a46582333004be83d495b60f465d5ff0ba31c
KJ Roelke <kj.roelke@gmail.com>
2023-05-11 13:24:50 -0500

    add console.log to import

--- wp-content/themes/labor-day/src/js/search/App.tsx
+++ wp-content/themes/labor-day/src/js/search/App.tsx
@@ -14,6 +14,7 @@ function App() {
 	const [ search, setSearch ] = useState( '' );
 	useEffect( () => {
 		if ( '' === search ) {
+			console.log( Model.getPosts );
 			const data = Model.getPosts()
 				.then( ( data ) => {
 					console.log( data );
