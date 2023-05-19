17b47a0f85a883a1dbbd3458c34363f8527f3ef7
KJ Roelke <kj.roelke@gmail.com>
2023-05-19 13:23:38 -0500

    init new modal

---
+++ wp-content/themes/labor-day/template-parts/modal-share-event.php
@@ -0,0 +1,25 @@
+<?php
+/** Share Event Modal
+ *
+ * @since 0.10.0
+ */
+
+?>
+<!-- Modal -->
+<div class="modal fade" id="shareEventModal" tabindex="-1" aria-labelledby="shareEventModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+	<div class="modal-content">
+	  <div class="modal-header">
+		<h1 class="modal-title fs-5" id="shareEventModalLabel">Share This Event</h1>
+		<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+	  </div>
+	  <div class="modal-body">
+		...
+	  </div>
+	  <div class="modal-footer">
+		<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		<button type="button" class="btn btn-primary">Save changes</button>
+	  </div>
+	</div>
+  </div>
+</div>
