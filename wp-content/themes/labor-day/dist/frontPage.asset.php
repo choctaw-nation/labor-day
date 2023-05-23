0a10e35aaafb2d3ac9e618a40321d6d71717a51b
KJ Roelke <kj.roelke@gmail.com>
2023-05-23 08:31:15 -0500

    bug fixes (internal review)

--- wp-content/themes/labor-day/dist/frontPage.asset.php
+++ wp-content/themes/labor-day/dist/frontPage.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '327ab5e483c121f27e48');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'c9e6621d0c6a8e82689c');
--- wp-content/themes/labor-day/dist/global.asset.php
+++ wp-content/themes/labor-day/dist/global.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '138955a25ead0b8df132');
+<?php return array('dependencies' => array(), 'version' => '91b3af6e1283395e7baf');
