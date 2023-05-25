345bc6f55f6002fcfdbdf852d35baaea46d60bfa
KJ Roelke <kj.roelke@gmail.com>
2023-05-25 15:25:20 -0500

    remove checkbox styles

--- wp-content/themes/labor-day/dist/search.asset.php
+++ wp-content/themes/labor-day/dist/search.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '0387767b0d005f6e93ad');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'cb2c8217660452fbb0f1');
