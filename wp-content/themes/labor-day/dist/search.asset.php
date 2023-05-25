8058da2edc106f1e64f7937da6d4256f5455400b
KJ Roelke <kj.roelke@gmail.com>
2023-05-25 16:47:15 -0500

    create dropdowns

--- wp-content/themes/labor-day/dist/search.asset.php
+++ wp-content/themes/labor-day/dist/search.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => 'cb2c8217660452fbb0f1');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'd4d4c0ade6af3e06edd5');
--- wp-content/themes/labor-day/src/js/search/Components/SearchFilters.tsx
+++ wp-content/themes/labor-day/src/js/search/Components/SearchFilters.tsx
@@ -1,61 +1,68 @@
-import React from '@wordpress/element';
-import { EventFilter } from '../types/eventFilters';
+import React, { useState } from '@wordpress/element';
 
-export default function SearchFilters({
-	filters,
-	checkedFilters,
-	setCheckedFilters,
-}) {
-	function handleClick({ target }) {
-		if (!target.id) return;
-		if (checkedFilters.includes(target.id)) {
-			setCheckedFilters(
-				checkedFilters.filter((f) => {
-					return f !== target.id;
-				})
-			);
-		} else setCheckedFilters((prev) => [...prev, target.id]);
+export default function SearchFilters({ filters, setCheckedFilters }) {
+	const [selectedFilters, setSelectedFilters] = useState({});
+
+	function handleDropdownItemClick(categoryName, filterName) {
+		setSelectedFilters((prevSelectedFilters) => {
+			const newFilters = {
+				...prevSelectedFilters,
+				[categoryName]: filterName,
+			};
+			setCheckedFilters(Object.values(newFilters));
+			return newFilters;
+		});
 	}
 	return (
 		<div className="cno-event-search-filters">
-			{filters.map(({ type: { name, filters } }) => {
-				return (
-					<div
-						className="cno-event-search-filters__container"
-						onClick={(ev) => {
-							handleClick(ev);
-						}}
-						key={name}
-					>
-						<h4 className="cno-event-search-filters__title">
-							{name}
-						</h4>
-						<div className="cno-event-search-filters__filter-container">
-							{filters.map((filter: EventFilter, i: number) => {
-								return (
-									<div
-										className="cno-event-search-filters__filter"
+			{filters.map(({ type: { name, filters } }) => (
+				<div className="cno-event-search-filters__container" key={name}>
+					<h4 className="cno-event-search-filters__title">{name}</h4>
+					<div className="cno-event-search-filters__filter-container">
+						<div className="dropdown">
+							<button
+								className="btn__outline--secondary dropdown-toggle"
+								type="button"
+								id={`${name}-dropdown`}
+								data-bs-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false"
+							>
+								{selectedFilters[name]
+									? selectedFilters[name]
+									: 'Select an option'}
+							</button>
+							<div
+								className="dropdown-menu"
+								aria-labelledby={`${name}-dropdown`}
+							>
+								{filters.map((filter, i) => (
+									<button
 										key={i}
-									>
-										<input
-											type="checkbox"
-											name={filter.name}
-											id={filter.name}
-											checked={checkedFilters.includes(
+										onClick={() =>
+											handleDropdownItemClick(
+												name,
 												filter.name
-											)}
-											onChange={() => {}}
-										/>
-										<label htmlFor={filter.name}>
-											{filter.name}
-										</label>
-									</div>
-								);
-							})}
+											)
+										}
+									>
+										{filter.name}
+									</button>
+								))}
+							</div>
 						</div>
 					</div>
-				);
-			})}
+				</div>
+			))}
+			<button
+				className="btn__fill--secondary"
+				onClick={() => {
+					setCheckedFilters([]);
+					setSelectedFilters({});
+				}}
+			>
+				Reset Filters
+			</button>
 		</div>
 	);
 }
