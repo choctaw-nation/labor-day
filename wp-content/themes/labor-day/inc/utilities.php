09ad963ebf276f9f15c4a0cb2b0bcc842b694c46
KJ Roelke <kj.roelke@gmail.com>
2023-05-31 11:05:11 -0500

    set env

--- wp-content/themes/labor-day/inc/utilities.php
+++ wp-content/themes/labor-day/inc/utilities.php
@@ -4,3 +4,21 @@
  */
 
 define( 'lorem', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet' );
+
+
+function cno_set_environment() {
+    $serverName = $_SERVER['SERVER_NAME'];
+    
+    if (strpos($serverName, '.local') !== false) {
+        $_ENV['DEV'] = true;
+    } elseif (strpos($serverName, 'wpengine') !== false) {
+        $_ENV['STAGE'] = true;
+    } elseif (strpos($serverName, 'choctawnation.com') !== false) {
+        $_ENV['PROD'] = true;
+    } else {
+        return;
+    }
+}
+
+cno_set_environment();
+var_dump($_ENV);
