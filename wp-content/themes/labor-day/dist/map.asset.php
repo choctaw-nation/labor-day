495108554678bf39acff34fed48174941eb8a4d3
KJ Roelke <kj.roelke@gmail.com>
2023-06-07 16:38:18 -0500

    init new map

--- wp-content/themes/labor-day/dist/map.asset.php
+++ wp-content/themes/labor-day/dist/map.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'f49452aa216fd7aa7a7c');
+<?php return array('dependencies' => array(), 'version' => '3cbfe85207427f7507e0');
--- wp-content/themes/labor-day/src/js/map/map.js
+++ wp-content/themes/labor-day/src/js/map/map.js
@@ -5,43 +5,40 @@ class MapControls {
 	/** Attaches the two eventListeners to the approriate elements
 	 * @param {NodeList} nodeList the divs & inputs to attach eventListeners to
 	 */
-	handleNodeList( nodeList ) {
-		nodeList.forEach( ( node ) => {
-			node.addEventListener(
-				'click',
-				this.#handleVisibility.bind( this )
-			);
-			node.addEventListener( 'change', ( { target } ) => {
-				this.#toggleVisibility( target );
-			} );
-		} );
+	handleNodeList(nodeList) {
+		nodeList.forEach((node) => {
+			node.addEventListener('click', this.#handleVisibility.bind(this));
+			node.addEventListener('change', ({ target }) => {
+				this.#toggleVisibility(target);
+			});
+		});
 	}
 
 	/** The callback function that calls toggleVisibility */
-	#handleVisibility( ev ) {
+	#handleVisibility(ev) {
 		const { target } = ev;
-		const input = target.querySelector( 'input' );
-		if ( null === input ) return;
-		input.checked = ! input.checked;
-		this.#toggleVisibility( target, input );
+		const input = target.querySelector('input');
+		if (null === input) return;
+		input.checked = !input.checked;
+		this.#toggleVisibility(target, input);
 	}
 	/** Toggles the visibility of a `g` element
 	 * @param {HTMLElement} target A containing div
 	 * @param {HTMLElement} input the checkbox element
 	 */
-	#toggleVisibility( target, input = null ) {
-		if ( null === input ) {
-			const el = document.getElementById( target.dataset.id );
+	#toggleVisibility(target, input = null) {
+		if (null === input) {
+			const el = document.getElementById(target.dataset.id);
 			el.style.visibility = target.checked ? 'visible' : 'hidden';
 		} else {
-			const el = document.getElementById( target.dataset.id );
+			const el = document.getElementById(target.dataset.id);
 			el.style.visibility = input.checked ? 'visible' : 'hidden';
 		}
 	}
 }
 
 /** The class that grabs DOM elements and calls the appropriate method. */
-new ( class MapController extends MapControls {
+class MapController extends MapControls {
 	constructor() {
 		super();
 		this.#handleLayers();
@@ -53,18 +50,19 @@ new ( class MapController extends MapControls {
 		const layerToggleDivs = document.querySelectorAll(
 			'.map-toggles__areas .map-toggles__layer-toggle'
 		);
-		this.handleNodeList( layerToggleDivs );
+		this.handleNodeList(layerToggleDivs);
 	}
 	#handleIcons() {
 		const iconToggles = document.querySelectorAll(
 			'.map-toggles__icons .map-toggles__layer-toggle'
 		);
-		this.handleNodeList( iconToggles );
+		this.handleNodeList(iconToggles);
 	}
 	#handleLocations() {
 		const locationToggles = document.querySelectorAll(
 			'.map-toggles__locations .map-toggles__layer-toggle'
 		);
-		this.handleNodeList( locationToggles );
+		this.handleNodeList(locationToggles);
 	}
-} )();
+}
+const map = new MapController();
