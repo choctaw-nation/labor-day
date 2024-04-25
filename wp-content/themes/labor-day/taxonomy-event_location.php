40203ce507b0a78e3e79041ecdd601c51cd99c1b
KJ Roelke <kj.roelke@gmail.com>
2024-04-25 10:15:47 -0500

    update $term override and ignore warning

--- wp-content/themes/labor-day/taxonomy-event_location.php
+++ wp-content/themes/labor-day/taxonomy-event_location.php
@@ -23,34 +23,32 @@ get_header();
 	<section class="my-5 container">
 		<div class="row">
 			<?php
-				$map_snippets = array(
-					'ampitheater'                => 'Amphitheater',
-					'capitol-lawn'               => 'Capitol Lawn',
-					'capitol-museum'             => 'Capitol Museum',
-					'carnival'                   => 'Carnival',
-					'chapel'                     => 'Chapel',
-					'choctaw-village'            => 'Choctaw Village',
-					'corner-of-nashoba-and-council-house-road' => 'Corner of Nashoba and Council House Road',
-					'education-tent'             => 'Education Tent',
-					'greenhouse'                 => 'Greenhouse',
-					'healthy-living-tent'        => 'Healthy Living Tent',
-					'red-warrior-park'           => 'Red Warrior Park',
-					'softball-fields'            => 'Softball Fields',
-					'stickball-field'            => 'Stickball Field',
-					'tribal-membership-building' => 'Tribal Membership Building',
-				);
-				if ( ! array_key_exists( $term, $map_snippets ) ) {
-					echo "<div class='mt-5'>" . get_the_content() . '</div>';
-				} else {
-					if ( 'playground' === $term ) {
-						$term = 'playground-v2';
-					}
-					$src = get_stylesheet_directory_uri() . '/images/map-locations/' . $term . '.webp';
-					if ( $src ) {
-						echo "<img class='map-snippet col-12 col-lg-8' src='{$src}' alt='A snippet of the festival grounds map showing the location of the {$map_snippets[$term]}' />";
-					}
+			$map_snippets = array(
+				'ampitheater'                              => 'Amphitheater',
+				'capitol-lawn'                             => 'Capitol Lawn',
+				'capitol-museum'                           => 'Capitol Museum',
+				'carnival'                                 => 'Carnival',
+				'chapel'                                   => 'Chapel',
+				'choctaw-village'                          => 'Choctaw Village',
+				'corner-of-nashoba-and-council-house-road' => 'Corner of Nashoba and Council House Road',
+				'education-tent'                           => 'Education Tent',
+				'greenhouse'                               => 'Greenhouse',
+				'healthy-living-tent'                      => 'Healthy Living Tent',
+				'red-warrior-park'                         => 'Red Warrior Park',
+				'softball-fields'                          => 'Softball Fields',
+				'stickball-field'                          => 'Stickball Field',
+				'tribal-membership-building'               => 'Tribal Membership Building',
+			);
+			if ( ! array_key_exists( $term, $map_snippets ) ) {
+				echo "<div class='mt-5'>" . get_the_content() . '</div>';
+			} else {
+				$term = 'playground' === $term ? 'playground-v2' : $term; // phpcs:ignore
+				$src  = get_stylesheet_directory_uri() . '/images/map-locations/' . $term . '.webp';
+				if ( $src ) {
+					echo "<img class='map-snippet col-12 col-lg-8' src='{$src}' alt='A snippet of the festival grounds map showing the location of the {$map_snippets[$term]}' />";
 				}
-				?>
+			}
+			?>
 			<div class="col-12 col-lg-4 d-flex flex-column justify-content-center">
 				<p class="fs-6">This screenshot is taken from the interactive map.</p>
 				<div class="row my-4">
