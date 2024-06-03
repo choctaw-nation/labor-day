67f449b0020b7dfb9e3aeccc1ab167a916b31ac8
KJ Roelke <kj.roelke@gmail.com>
2024-06-03 14:10:19 -0500

    add recaptcha notices, bump packages and update deploy actions

--- wp-content/themes/labor-day/template-parts/page-contact.php
+++ wp-content/themes/labor-day/template-parts/page-contact.php
@@ -9,4 +9,9 @@
 <section class="container py-0">
 	<?php $form = get_field( 'form' ); ?>
 	<?php echo do_shortcode( $form ); ?>
+	<div class="row">
+		<div class='col'>
+			<?php get_template_part( 'template-parts/registrations/content-recaptcha-notice' ); ?>
+		</div>
+	</div>
 </section>
