6d9a7bb9718534066a97deccfc096fba4d39a96a
KJ Roelke <kj.roelke@gmail.com>
2024-07-08 14:40:54 -0500

    undo meta tag alteration

--- wp-content/themes/labor-day/header.php
+++ wp-content/themes/labor-day/header.php
@@ -14,6 +14,7 @@ use ChoctawNation\Navwalker;
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
