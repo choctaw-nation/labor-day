45e7d9ec17e07814dcb389bc7b0d75e912519455
KJ Roelke <kroelke@choctawnation.com>
2025-11-17 14:30:48 -0600

    block ui improvements

--- wp-content/themes/labor-day/src/js/blocks/registrations-swiper-block/hooks/useRegistrationPages.tsx
+++ wp-content/themes/labor-day/src/js/blocks/registrations-swiper-block/hooks/useRegistrationPages.tsx
@@ -1,24 +1,16 @@
-import { useSelect } from '@wordpress/data';
-import { store as coreDataStore, Post } from '@wordpress/core-data';
+import { Post, useEntityRecords } from '@wordpress/core-data';
 import { RegistrationPage } from '../types';
 
 export default function useRegistrationPages() {
-	const registrationPages = useSelect( ( select ) => {
-		const maybeRegistrationPage = select( coreDataStore ).getEntityRecords(
-			'postType',
-			'page',
-			{
-				slug: 'registrations',
-				per_page: 1,
-				_fields: 'id',
-			}
-		) as Post[] | null;
-		if ( ! maybeRegistrationPage ) {
-			return [];
-		}
-		const registrationsPage = maybeRegistrationPage[ 0 ].id;
-		return select( coreDataStore ).getEntityRecords( 'postType', 'page', {
-			parent: registrationsPage,
+	const { records, isResolving: isParentResolving } =
+		useEntityRecords< Post >( 'postType', 'page', {
+			slug: 'registrations',
+			per_page: 1,
+			_fields: 'id',
+		} );
+	const parentPageId = records?.length ? records[ 0 ].id : undefined;
+	const { records: childRecords, isResolving: isChildrenResolving } =
+		useEntityRecords< RegistrationPage >( 'postType', 'page', {
 			per_page: 100,
 			_fields: 'id,title,_embedded,_links,acf,excerpt',
 			_embed: 'wp:featuredmedia',
@@ -26,7 +18,9 @@ export default function useRegistrationPages() {
 			order: 'asc',
 			status: 'publish',
 			orderby: 'title',
+			parent: parentPageId,
 		} );
-	}, [] ) as RegistrationPage[] | null;
-	return { registrationPages };
+	const registrationPages = childRecords;
+	const isLoading = isParentResolving || isChildrenResolving;
+	return { registrationPages, isLoading };
 }
